3af4317c21a86f5eaa2864a490c04f2ae9aa6bea
huseyinSUBAS <subasshuseyin@gmail.com>
2022-11-12 01:35:30 +0300

    zaman js ödevi-1

---
+++ saat.js
@@ -0,0 +1,17 @@
+let username=prompt("---ADINIZ--");
+document.getElementById("username").innerHTML=username;
+
+
+
+function zaman(){
+
+    var zmn=new Date()
+    var h=zmn.getHours()
+    var m=zmn.getMinutes()
+    var s=zmn.getSeconds()
+    var day=zmn.getDay()
+    let days=["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
+    document.getElementById("zamann").innerHTML=h + ":"+m+":"+s+"  "+days[day];
+}
+setInterval(zaman,1000);
+zaman();
